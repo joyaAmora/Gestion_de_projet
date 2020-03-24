367003c7dfa71f62e45102369fdd989025ea3968
blackxthink <blackxthink@gmail.com>
2020-03-24 18:52:35 -0400

    site design up to date

---
+++ site2/items/chart24h.js
@@ -0,0 +1,97 @@
+var heure1 = "5:00";
+var heure2 = "7:00";
+var heure3 = "9:00";
+var heure4 = "11:00";
+var heure5 = "13:00";
+var heure6 = "15:00";
+var heure7 = "17:00";
+var heure8 = "19:00";
+var heure9 = "21:00";
+var heure10 = "23:00";
+var heure11 = "1:00";
+var heure12 = "3:00";
+
+//---------------------------
+
+var temp1 = 22.3;
+var temp2 = 22.4;
+var temp3 = 21.7;
+var temp4 = 21.0;
+var temp5 = 20.7;
+var temp6 = 20.6;
+var temp7 = 20.1;
+var temp8 = 19.8;
+var temp9 = 19.0;
+var temp10 = 23.8;
+var temp11 = 24.1;
+var temp12 = 25.9;
+//---------------------------
+
+var chart24h = $("#ch1");
+
+chart24h.append(
+  "<canvas id='chart24h'></canvas>"
+); /*chart24h.append("<canvas id='chart24h' width='500' height=300'></canvas>");*/
+
+var ctx = $("#chart24h");
+var myChart = new Chart(ctx, {
+  type: "line",
+  data: {
+    labels: [
+      heure1,
+      heure2,
+      heure3,
+      heure4,
+      heure5,
+      heure6,
+      heure7,
+      heure8,
+      heure9,
+      heure10,
+      heure11,
+      heure12
+    ],
+    datasets: [
+      {
+        label: "Temperature 24h",
+        data: [
+          temp1,
+          temp2,
+          temp3,
+          temp4,
+          temp5,
+          temp6,
+          temp7,
+          temp8,
+          temp9,
+          temp10,
+          temp11,
+          temp12
+        ],
+        backgroundColor: ["rgba(255, 99, 132, 0)"],
+        borderColor: ["rgba(255, 99, 132, 1)"],
+        borderWidth: 4
+      }
+    ]
+  },
+  options: {
+    responsive: true,
+    maintainAspectRatio: false,
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true
+          }
+        }
+      ]
+    }
+  }
+});
+
+var testCalcul = $("#gaugeRow1").height();
+var gaugeGauche2 = $("#gaugeGauche2").height();
+var heighttotal = gaugeGauche2 + (testCalcul - gaugeGauche2);
+console.log(heighttotal);
+
+$("#chartsRow1").height(heighttotal);
---
+++ site2/items/chartHumidity.js
@@ -0,0 +1,97 @@
+var heure1 = "5:00";
+var heure2 = "7:00";
+var heure3 = "9:00";
+var heure4 = "11:00";
+var heure5 = "13:00";
+var heure6 = "15:00";
+var heure7 = "17:00";
+var heure8 = "19:00";
+var heure9 = "21:00";
+var heure10 = "23:00";
+var heure11 = "1:00";
+var heure12 = "3:00";
+
+//---------------------------
+
+var humidity1 = 58;
+var humidity2 = 50;
+var humidity3 = 52;
+var humidity4 = 43;
+var humidity5 = 51;
+var humidity6 = 70;
+var humidity7 = 41;
+var humidity8 = 57;
+var humidity9 = 37;
+var humidity10 = 75;
+var humidity11 = 71;
+var humidity12 = 56;
+//---------------------------
+
+var chartHumidity = $("#ch2");
+
+chartHumidity.append(
+  "<canvas id='chartHumidity'></canvas>" /* "<canvas id='chartHumidity' width='500' height=300'></canvas>"*/
+);
+
+var ctx2 = $("#chartHumidity");
+var myChart2 = new Chart(ctx2, {
+  type: "line",
+  data: {
+    labels: [
+      heure1,
+      heure2,
+      heure3,
+      heure4,
+      heure5,
+      heure6,
+      heure7,
+      heure8,
+      heure9,
+      heure10,
+      heure11,
+      heure12
+    ],
+    datasets: [
+      {
+        label: "Humidity 24h",
+        data: [
+          humidity1,
+          humidity2,
+          humidity3,
+          humidity4,
+          humidity5,
+          humidity6,
+          humidity7,
+          humidity8,
+          humidity9,
+          humidity10,
+          humidity11,
+          humidity12
+        ],
+        backgroundColor: ["rgba(255, 99, 132, 0)"],
+        borderColor: ["rgba(0,191,255, 1)"],
+        borderWidth: 4
+      }
+    ]
+  },
+  options: {
+    responsive: true,
+    maintainAspectRatio: false,
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true
+          }
+        }
+      ]
+    }
+  }
+});
+
+var testCalcul = $("#gaugeRow1").height();
+var gaugeGauche2 = $("#gaugeGauche2").height();
+var heighttotal = gaugeGauche2 + (testCalcul - gaugeGauche2);
+console.log(heighttotal);
+
+$("#chartsRow2").height(heighttotal);
