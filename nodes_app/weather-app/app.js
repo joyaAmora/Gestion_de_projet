2f1c057f0b394274271575fd7724db9ba151ecf6
Jade <jade.phaneuf@gmail.com>
2020-04-15 11:16:56 -0400

    projets de formation

---
+++ nodes_app/weather-app/app.js
@@ -0,0 +1,43 @@
+//const request = require('request')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+
+const address = process.argv[2]
+// const url = 'https://api.darksky.net/forecast/18981b1f9bc4d9e2838e9cf103bc29ba/37.8267,-122.4233?units=si&lang=fr'
+
+// request({ url: url, json: true }, (error, response) =>{
+//     if(error)
+//         console.log('Unable to connect to weather service!')
+//     else if(response.body.error)
+//     {
+//         console.log('Unable to find location')
+//     }
+//     else
+//     {
+//         console.log(response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degrees and there is ' + 
+//         response.body.currently.precipProbability + ' % of rain')
+//     }
+    
+// }) 
+
+if(!address)
+    console.log('Please provide an address')
+else{
+    geocode(address, (error, {latitude, longitude, location}) => {
+        if(error)
+           return console.log(error)
+   
+       forecast(latitude, longitude, (error, forecastData) => {
+           if(error)
+               return console.log(error)
+           else
+           console.log(location)
+           console.log(forecastData)
+         })
+       
+    })
+}
+
+
+
+
