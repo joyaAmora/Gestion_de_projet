b05949d21b033e0d5780a4d9daff55eefa9ed6d7
tomcp19 <60187795+tomcp19@users.noreply.github.com>
2020-04-21 17:00:05 -0400

    Ajout du calcul du % d'humidité en decimal

--- soilmoisture/soilmoisture.ino
+++ soilmoisture/soilmoisture.ino
@@ -4,6 +4,7 @@ int Air = 572; //valeur de la sonde a sec à l'air libre lors de la calibration 
 int Eau = 310; //valeur de la sonde dans un verre d'eau lors de la calibration initiale
 int intervals = (Air - Eau)/3; // intervalle pour décider si humide ou sec
 int soilMoistureValue = 0;
+int ActualMoisture;//%, entre 0 et 1
 
 void setup() {
 Serial.begin(9600); // lecture sur port serie avec un baud rate de 9600 bps
@@ -31,6 +32,13 @@ delay(100);
 */
 
 soilMoistureValue = analogRead(A0); //lecture de la sonde dans le sol
+
+if (soilMoistureValue <  WaterValue) {soilMoistureValue = WaterValue+1;}//pour eviter les division par 0
+if (soilMoistureValue >  AirValue) {soilMoistureValue = AirValue-1;}
+
+//100% humidité == 310, 0% == 572 (plus cest haut plus c'est sec). 262 pts a diviser sur une echelle de 100, donc 2,62 pts par %. Si on adapte:
+AcTualMoisture = 100+(WaterValue-soilMoistureValue)/2.62 //ex: 441 serait 50%. (100+(310-441)/2.62)/100 = 0.50
+    Serial.println("Humidité du sol (%) :", AcTualMoisture * 100);
 
 if(soilMoistureValue > WaterValue && soilMoistureValue < (WaterValue + intervals))
   {
